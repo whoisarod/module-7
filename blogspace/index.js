bcd459adac4e43f1d38ee8699da2af5e1b23b1af
Angel Rodriguez <angelrodri@hotmail.com>
2022-01-14 16:08:52 -0800

    added challenges for REST API

--- blogspace/index.js
+++ blogspace/index.js
@@ -45,3 +45,28 @@ form.addEventListener("submit", function (e) {
             form.reset()
     })
 } )
+
+/**
+ * Challenge: GET all the comments from the blog post with ID of 2 and log to the console
+ * 
+ * BaseURL: https://apis.scrimba.com/jsonplaceholder/
+ * Endpoint: ??? (Check JSON Placeholder docs: https://jsonplaceholder.typicode.com/guide/ and look for the "Listing nested resources" section)
+ */
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts/2/comments", {method: "GET"})
+    .then(res => res.json())
+    .then(data => console.log(data))
+
+/**
+ * Challenge part 1: GET the current weather for your city with 
+ * the Open Weather API and log it to the console.
+ * 
+ * BaseURL: https://apis.scrimba.com/openweathermap/data/2.5/
+ * Endpoint: /weather
+ * Query: ??? (https://openweathermap.org/current)
+    * NOTE: It says you need to include `appid` in your query, but you can skip that this time
+ */
+
+fetch("https://apis.scrimba.com/openweathermap/data/2.5/weather/?zip=90033,us&units=imperial")
+    .then(res => res.json())
+    .then(data => console.log(data))
