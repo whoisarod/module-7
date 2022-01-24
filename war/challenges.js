c8bb385c1f2318c8db7d18b1830a7724339acc0d
Angel Rodriguez <angelrodri@hotmail.com>
2022-01-24 14:57:50 -0800

    updated all files, added img files for war game

--- war/challenges.js
+++ war/challenges.js
@@ -139,3 +139,133 @@ const finalResult = voters.filter(voter => voter.voted)
     console.log(finalResult);
 
 // Final result: ["joe@joe.com", "jane@jane.com"]
+
+let deckId
+
+function handleClick() {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            deckId = data.deck_id
+        })
+}
+
+document.getElementById("new-deck").addEventListener("click", handleClick)
+/**
+ * Challenge
+ * 
+ * Background:
+ * The Deck of Cards API expects us to provide the deck id 
+ * of the deck we're playing with so it can remember which
+ * cards we've already drawn, how many are remaining in the
+ * deck, etc.
+ * 
+ * Task: save the deck_id from the returned data to a local
+ * variable so we can use it later
+ */
+
+ let deckId
+
+function handleClick() {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            deckId = data.deck_id
+        })
+}
+
+document.getElementById("new-deck").addEventListener("click", handleClick)
+/**
+ * Challenge
+ * 
+ * Task: Using the saved deckId, draw 2 new cards from the deck
+ * 
+ * Docs for original Deck of Cards API: https://deckofcardsapi.com/#draw-card
+ * BaseUrl you'll use: https://apis.scrimba.com/deckofcards/api/deck/
+ * (that will replace the base url of https://deckofcardsapi.com/api/deck/)
+ * that you'll see in the deck of cards API docs.
+ * 
+ * 1. Create a new button that, when clicked, draws 2 cards from the deckId
+ * you have saved
+ *      Note: you'll need to get a new deck every time you refresh the page,
+ *      since you're only saving your deckId in a local variable right now
+ * 2. Log those 2 cards to the console
+ */
+
+function drawTwoCards() {
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data = console.log(data))
+}
+
+document.getElementById('drawNewCards').addEventListener('click', drawTwoCards);
+
+//u can also use an arrow function when drawing two new cards
+document.getElementById("draw-cards").addEventListener("click", () => {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+})
+
+let deckId
+
+function handleClick() {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            deckId = data.deck_id
+        })
+}
+
+document.getElementById("new-deck").addEventListener("click", handleClick)
+
+document.getElementById("draw-cards").addEventListener("click", () => {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            let img = data.images.svg
+
+        })
+})
+/**
+ * Challenge:
+ * 
+ * Display the images of the 2 cards you drew in the browser.
+ * Probably best to use `innerHTML` to insert a couple <img> elements
+ * on the page.
+ */
+
+ let deckId
+
+ function handleClick() {
+     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+         .then(res => res.json())
+         .then(data => {
+             console.log(data)
+             deckId = data.deck_id
+         })
+ }
+ 
+ document.getElementById("new-deck").addEventListener("click", handleClick)
+ 
+ document.getElementById("draw-cards").addEventListener("click", () => {
+     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+         .then(res => res.json())
+         .then(data => {
+             console.log(data.cards)
+             document.getElementById("cards").innerHTML = `
+                 <img src=${data.cards[0].image} />
+                 <img src=${data.cards[1].image} />
+             `
+         })
+ })
+ /**
+  * Challenge:
+  * 
+  * Display the images of the 2 cards you drew in the browser.
+  * Probably best to use `innerHTML` to insert a couple <img> elements
+  * on the page.
+  */
--- war/index.js
+++ war/index.js
@@ -1,18 +1,24 @@
-function getNewDeck() {
-    fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/', {method: "GET"})
-       .then(res => res.json())
-       .then(data => console.log(data))
+let deckId
+
+function handleClick() {
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            deckId = data.deck_id
+        })
 }
 
-document.getElementById('btn').addEventListener('click', getNewDeck)
+document.getElementById("new-deck").addEventListener("click", handleClick)
 
-const promise = fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
-const secondPromise = promise.then(res => res.json())
-console.log(secondPromise)
-//Promise Chaining
-// .then(data => console.log(data))
-
-/**
- * Mini challenge: Figure out what `promise.then()` returns! 
- * Save the result to a variable and log it to the console.
- */
+document.getElementById("draw-cards").addEventListener("click", () => {
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.cards)
+            document.getElementById("cards").innerHTML = `
+                <img src=${data.cards[0].image} />
+                <img src=${data.cards[1].image} />
+            `
+        })
+})
