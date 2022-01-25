58bd0bcbbfc35af81c81cd74db06e88a9458fb20
Angel Rodriguez <angelrodri@hotmail.com>
2022-01-25 11:19:03 -0800

    updated css and html to include card placements pulled from the api

--- war/index.js
+++ war/index.js
@@ -1,5 +1,9 @@
 let deckId
 
+const cardsContainter = document.getElementById("cards");
+const newDeckBtn = document.getElementById("new-deck");
+const drawCardBtn = document.getElementById("draw-cards");
+
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
@@ -9,16 +13,34 @@ function handleClick() {
         })
 }
 
-document.getElementById("new-deck").addEventListener("click", handleClick)
+newDeckBtn.addEventListener("click", handleClick)
 
-document.getElementById("draw-cards").addEventListener("click", () => {
+drawCardBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            console.log(data.cards)
-            document.getElementById("cards").innerHTML = `
-                <img src=${data.cards[0].image} />
-                <img src=${data.cards[1].image} />
+            
+            cardsContainter.children[0].innerHTML = `
+            <img src=${data.cards[0].image} class='card'/>
+            `
+            cardsContainter.children[1].innerHTML = `
+            <img src=${data.cards[1].image} class='card'/>
             `
         })
 })
+
+function determineCardWinner(card1, card2) {
+    const valueOptions = [
+        "2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"
+    ]
+    const card1ValueIndex = valueOptions.indexOf(card1.value)
+    const card2ValueIndex = valueOptions.indexOf(card2.value)
+
+    if (card1ValueIndex > card2ValueIndex) {
+        console.log('Card 1 Wins!');
+    } else if (card1ValueIndex < card2ValueIndex) {
+        console.log('Card 2 Wins!');
+    } else {
+        console.log("It's a tie!");
+    }
+}
